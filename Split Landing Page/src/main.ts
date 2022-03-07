00a86a9e5866803d090c6ad7e02519f9a5ee45c1
prakashchandra3786@gmail.com <prakashchandra3786@gmail.com>
2022-03-07 08:33:37 +0530

    Split landing page done

--- Split Landing Page/src/main.ts
+++ Split Landing Page/src/main.ts
@@ -5,19 +5,19 @@ const xbox = document.querySelector(".xbox");
 
 allSection?.forEach((section) => {
   section.addEventListener("mouseover", (e) => {
-    if (e.target?.closest(".ps5") === ps5) {
-      main.classList.add("activeLeft");
+    if ((<HTMLInputElement>e.target)?.closest(".ps5") === ps5) {
+      main?.classList.add("activeLeft");
     }
-    if (e.target?.closest(".xbox") === xbox) {
-      main.classList.add("activeRight");
+    if ((<HTMLInputElement>e.target)?.closest(".xbox") === xbox) {
+      main?.classList.add("activeRight");
     }
   });
   section.addEventListener("mouseleave", (e) => {
-    if (e.target?.closest(".ps5") === ps5) {
-      main.classList.remove("activeLeft");
+    if ((<HTMLInputElement>e.target)?.closest(".ps5") === ps5) {
+      main?.classList.remove("activeLeft");
     }
-    if (e.target?.closest(".xbox") === xbox) {
-      main.classList.remove("activeRight");
+    if ((<HTMLInputElement>e.target)?.closest(".xbox") === xbox) {
+      main?.classList.remove("activeRight");
     }
   });
 });
