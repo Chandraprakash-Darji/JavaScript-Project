2ed6c93266e732aca6504d694966f7c0ef7fe00e
prakashchandra3786@gmail.com <prakashchandra3786@gmail.com>
2022-03-20 15:44:26 +0530

    Improved Interesection fun

--- Scroll Animation/src/main.ts
+++ Scroll Animation/src/main.ts
@@ -1,23 +1,16 @@
 const li = document.querySelectorAll("li");
 
-const allSection = document.querySelectorAll(".section");
-const revelLi = (entries) => {
+const revelLi = (entries: IntersectionObserverEntry[]) => {
   entries.forEach((entry) => {
-    // liObserver.observe(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
+    if (entry.isIntersecting) entry.target.classList.add("show");
+    else entry.target.classList.remove("show");
   });
-  // const [entry] = entries;
-  console.log(entries);
 };
+
 const liObserver = new IntersectionObserver(revelLi, {
-  root: null,
-  threshold: 1,
-  rootMargin:"-30px"
+  rootMargin: "-50px", 
 });
+
 li.forEach((li1) => {
   liObserver.observe(li1);
 });
