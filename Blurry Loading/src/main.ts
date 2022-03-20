5d65946e98b6aa04ef6757efe6195b2b2cc4cb1e
prakashchandra3786@gmail.com <prakashchandra3786@gmail.com>
2022-03-20 14:51:32 +0530

    fixed blur name

--- Blurry Loading/src/main.ts
+++ Blurry Loading/src/main.ts
@@ -1,14 +1,14 @@
 const main = document.querySelector("main") as HTMLElement;
 const p = document.querySelector("p") as HTMLElement;
-let blur: number = 15;
+let blurNo: number = 15;
 p.style.backdropFilter = `blur(${blur}px)`;
 
 const blurLoad = setInterval(() => {
-  p.style.backdropFilter = `blur(${blur}px)`;
-  p.textContent = `${Math.trunc((blur * 100) / 15)}%`;
-  if (blur < 4) p.style.opacity = (blur * 1) / 15;
-  if (blur <= 0) {
+  p.style.backdropFilter = `blur(${blurNo}px)`;
+  p.textContent = `${Math.trunc((blurNo * 100) / 15)}%`;
+  if (blurNo < 4) p.style.opacity = `${(blurNo * 1) / 15}`;
+  if (blurNo <= 0) {
     clearInterval(blurLoad);
     p.textContent = "";
-  } else blur = Number((blur - 0.1).toFixed(2));
+  } else blurNo = Number((blurNo - 0.1).toFixed(2));
 }, 55);
