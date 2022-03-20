a03f08702eda393457cf9f49af2a5ddc9db7ce9e
prakashchandra3786@gmail.com <prakashchandra3786@gmail.com>
2022-03-20 15:42:21 +0530

    removed dark style and usless comment

--- Form wave/src/main.ts
+++ Form wave/src/main.ts
@@ -1,10 +1,7 @@
-const emailInput = document.querySelector("#email");
-const passwordInput = document.querySelector("#password");
-const emailLabel = document.querySelector(".emailLabel");
-const passwordLabel = document.querySelector(".passwordLabel");
-import "./style.css";
-// const ps5 = document.querySelector(".ps5");
-// const xbox = document.querySelector(".xbox");
+const emailInput = document.querySelector("#email") as HTMLInputElement;
+const passwordInput = document.querySelector("#password") as HTMLInputElement;
+const emailLabel = document.querySelector(".emailLabel") as HTMLLabelElement;
+const passwordLabel = document.querySelector(".passwordLabel") as HTMLLabelElement;
 
 emailInput?.addEventListener("focus", function () {
   emailLabel?.classList.add("active");
