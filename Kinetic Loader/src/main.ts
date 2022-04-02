a2f0ed38ce349b4dc503d6c857529f06745bc8cd
prakashchandra3786@gmail.com <prakashchandra3786@gmail.com>
2022-04-02 12:38:40 +0530

    Kinetic loader

---
+++ Kinetic Loader/src/main.ts
@@ -0,0 +1,7 @@
+const canvasEl = document.getElementById("canvas") as HTMLCanvasElement;
+const selectColor = document.getElementById("selectColor") as HTMLInputElement;
+const sizeEl = document.querySelector(".size");
+const minusEl = document.querySelector(".minus");
+const plusEl = document.querySelector(".plus");
+const crossEl = document.querySelector(".cross");
+const eraseEl = document.querySelector(".erase");
